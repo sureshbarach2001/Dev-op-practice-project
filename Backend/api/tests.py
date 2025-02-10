a03a93850d2359b0a813f16ac2aa56685540f6f3
sureshbarach2001 <saink4831@gmail.com>
2025-02-11 03:52:13 +0500

    CI/CD

--- Backend/api/tests.py
+++ Backend/api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class SimpleTest(TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_string_formatting(self):
+        self.assertEqual("Hello, {}".format("World"), "Hello, World")
